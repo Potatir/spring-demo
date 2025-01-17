11c6e7fa614703d7b6351ffa1510b1076e13e98b
Александр <sanyachurilov5@gmail.com>
2025-01-17 14:39:39 +0500

    github

---
+++ src/main/java/com/example/nagithub/NagithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.nagithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nagithub/NagithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nagithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
